7950b16b1355b5c36d6fe8aa6e8d93e3c00a36c5
dev-mehedi-github <aiubmehedi20@gmail.com>
2026-07-07 21:17:20 +0600

    Refactor collect_data and collect_DOB functions for improved clarity and logging

--- Mid Terrm/Theory_W5_7July/Javascript/Script.js
+++ Mid Terrm/Theory_W5_7July/Javascript/Script.js
@@ -12,6 +12,15 @@ var text=a.length;
 console.log(text);
 
 function collect_data(){
+    let pname=document.getElementById("name").value;
+    console.log(pname);
+    let collectDOB = collect_DOB();
+    return false;
+}
 
+function collect_DOB()
+{
+    let dob=document.getElementById("DOB").value;
+    console.log(dob);
     return false;
 }
