bb0d1aa8c9f2dd5f1e36d041f066f057010ab87a
dev-mehedi-github <aiubmehedi20@gmail.com>
2026-07-11 14:36:16 +0600

    Add initial calculations and logging for sum and subtraction of variables

--- Mid Terrm/Lab_Task1_28Jun/Javascript/script.js
+++ Mid Terrm/Lab_Task1_28Jun/Javascript/script.js
@@ -1,3 +1,12 @@
+console.log("HTML Connected");
+let a=10.50;
+var b=20;
+sum=a+b;
+sub=a-b;
+x=a+b;
+console.log("Sum of a and b is: "+sum);
+console.log("Subtraction of a and b is: "+sub);
+
 function collect_data(){
     let collectName= collect_Name();
     let collectEmail= collect_Email();
