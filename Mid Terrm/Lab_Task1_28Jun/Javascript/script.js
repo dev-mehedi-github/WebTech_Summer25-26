039458cb42702fd88960cc8f95d946d7c39d4fa5
dev-mehedi-github <aiubmehedi20@gmail.com>
2026-07-11 14:11:25 +0600

    Add email collection functionality to collect_data function

--- Mid Terrm/Lab_Task1_28Jun/Javascript/script.js
+++ Mid Terrm/Lab_Task1_28Jun/Javascript/script.js
@@ -1,10 +1,30 @@
 function collect_data(){
     let collectName= collect_Name();
+    let collectEmail= collect_Email();
+
     return false;
 }
 
 function collect_Name(){
     let sName=document.getElementById("name").value;
     console.log(sName);
+    return false;
+}
+
+function collect_Email(){
+    let sEmail= document.getElementById("email").value;
+    console.log(sEmail);
+    return false;
+}
+
+function collect_Password(){
+    let sPass= document.getElementById("pass").value;
+    console.log(sPass);
+    return false;
+}
+
+function collect_Age(){
+    let sAge= document.getElementById("age").value;
+    console.log(sAge);
     return false;
 }
