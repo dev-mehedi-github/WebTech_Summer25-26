f1e43c30670f95c04467400527278e81180673ad
dev-mehedi-github <aiubmehedi20@gmail.com>
2026-07-11 14:14:50 +0600

    Add missing data collection functions to collect_data

--- Mid Terrm/Lab_Task1_28Jun/Javascript/script.js
+++ Mid Terrm/Lab_Task1_28Jun/Javascript/script.js
@@ -1,7 +1,10 @@
 function collect_data(){
     let collectName= collect_Name();
     let collectEmail= collect_Email();
-
+    let collectPass= collect_Password();
+    let collectAge= collect_Age();
+    let collectExpDate= collect_ExpDate();
+    let collectPhone= collect_Phone();
     return false;
 }
 
@@ -28,3 +31,16 @@ function collect_Age(){
     console.log(sAge);
     return false;
 }
+
+function collect_ExpDate(){
+    let sExpD= document.getElementById("ExpDate").value;
+    console.log(sExpD);
+    return false;
+}
+
+function collect_Phone(){
+    let sPhone= document.getElementById("phone").value;
+    console.log(sPhone);
+    return false;
+}
+
