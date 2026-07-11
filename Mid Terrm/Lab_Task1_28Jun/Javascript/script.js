8f2487860fbef4c6ae206fa5005b9d03ffb342db
dev-mehedi-github <aiubmehedi20@gmail.com>
2026-07-11 13:46:55 +0600

    Fix collect_data and collect_Name functions to properly retrieve and log name input

--- Mid Terrm/Lab_Task1_28Jun/Javascript/script.js
+++ Mid Terrm/Lab_Task1_28Jun/Javascript/script.js
@@ -1,4 +1,10 @@
 function collect_data(){
+    let collectName= collect_Name();
+    return false;
+}
 
+function collect_Name(){
+    let sName=document.getElementById("name").value;
+    console.log(sName);
     return false;
 }
