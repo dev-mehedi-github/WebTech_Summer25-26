d2f3d60c9b31414c56a2e1bffc59073c3afa8ed0
dev-mehedi-github <aiubmehedi20@gmail.com>
2026-08-06 22:07:40 +0600

    Feat: Implement initial structure for IndexValidation.php with form handling logic

---
+++ Final Term/zPractice/HW1(php_js_Validation)/Controler/IndexValidation.php
@@ -0,0 +1,17 @@
+<?php
+
+$name = "";
+$email = "";
+$website = "";
+$comment = "";
+$gender = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = trim($_POST["name"] ?? "");
+    $email = trim($_POST["email"] ?? "");
+    $website = trim($_POST["website"] ?? "");
+    $comment = trim($_POST["comment"] ?? "");
+    $gender = trim($_POST["gender"] ?? "");
+
+}
+?>
