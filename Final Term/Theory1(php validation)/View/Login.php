0438ccbe3ef5782ee6df89a14ffa4f1ccec8e096
dev-mehedi-github <aiubmehedi20@gmail.com>
2026-08-06 20:17:21 +0600

    Fix: Correct spelling in include statement and improve validation messages in Login.php

--- Final Term/Theory1(php validation)/View/Login.php
+++ Final Term/Theory1(php validation)/View/Login.php
@@ -1,65 +1,60 @@
 <?php
-include "../Controller/Loginvaliadation.php";
+include "../Controller/Loginvalidation.php";
 ?>
 <!DOCTYPE html>
 <html>
     <head>
-    <title>LogIn Page</title>
-    <script>
-        function collect_data()
-        {
-            let name = document.getElementById("name").value.trim();
-            let password = document.getElementById("password").value.trim();
-            let valid= true;
-            let message ="";
-
-            if(name.length<5)
+        <title> Login Page </title>
+        <script>
+            function collect_data()
             {
-                message+="User Name Must Be 5 charecter";
-                valid=false;
-            }
-            if(password.length<5)
-            {
-                message+="password Must Be 5 charecter";
-                valid=false;
-            }
+                let name = document.getElementById("name").value.trim();
+                let password = document.getElementById("password").value.trim();
+                let valid = true;
+                let message="";
+                if(name.length <5)
+                {
+                    message+="User Name Should be 5 Char";
+                    valid = false;
+                }
+                if(password.length <5)
+                {
+                    message+="Password Must be 5 Char";
+                    valid = false;
+                }
+                if(!valid)
+                {
+                    alert(message);
+                }
+                return valid;
 
-            if(!valid)
-            {
-                alert(message);
             }
-
-            return valid;
-        }
-    </script>
+        </script>
     </head>
     <body>
-    <form method="post" onsubmit="return collect_data()" action="">
-    <table>
-        <tr>
-            <td>
-                <label for="Username"> User Name: </label>
+       <form method="post" action="" onsubmit="return collect_data()"> 
+        <table>
+            <tr>
+                <td> <label for="username"> User Name: </label></td>
+                <td> <input type="text" id="name" name="name">
+                <?php echo $name ?>
             </td>
-            <td> 
-                <input type ="text" id="name" name="name">
-                <?php echo $name; ?>
-            </td>
-        </tr>
-        <tr>
-            <td> <label for="pass"> Password: </label></td>
-            <td> <input type ="password" id="password" name="password">
-            <?php echo $password; ?>
-        </td> 
-        </tr>
-        <tr>
-            <td>
-                <input type="submit" id="submit" name="submit" value="LogIn">
-                <input type="reset" id="reset" name="reset">
+            </tr>
 
+             <tr>
+                <td> <label for="pass"> Password: </label></td>
+                <td> <input type="password" id="password" name="password">
+                <?php echo $password ?>
             </td>
-        </tr>
-    </table> 
+            </tr>
 
-    </form>
+            <tr>
+                <td>
+                    <input type="submit" id="submit" value="LogIn">
+                    <input type="reset" id="reset">
+                </td>
+            </tr>
+        </table>
+       </form>
     </body>
 </html>
